32ca354afa5d42deb66cc50d2ea6d17e1ae48521
Wasif Hyder <wasif.hyder@gmail.com>
2020-02-23 19:16:52 +0800

    Updated meta info including name, description etc

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Wasif Hyder`,
+    description: `The blog of Wasif Hyder. Fullstack Developer, Artist, Comedian`,
+    author: `@wasif_hyder`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
